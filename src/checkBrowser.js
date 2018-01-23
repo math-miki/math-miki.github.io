cf5deaf96502e7c5a8c0750467bcf7b77bddb9ff
math-miki <math.micky.dtl@gmail.com>
2018-01-23 17:00:21 +0900

    added js file checks browser

---
+++ src/checkBrowser.js
@@ -0,0 +1,7 @@
+var userAgent = window.navigator.userAgent.toLowerCase();
+
+if(userAgent.indexOf('msie') != -1 || userAgent.indexOf('trident') != -1) {
+    window.location.href = '../changeBrowser.html'
+} else if(userAgent.indexOf('edge') != -1) {
+    window.location.href = '../changeBrowser.html'
+}
