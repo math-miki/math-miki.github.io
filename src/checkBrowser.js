fffb384c2a8de2763cada09f2f928a0285dd8326
math-miki <math.micky.dtl@gmail.com>
2018-01-23 17:04:35 +0900

    chrome test

--- src/checkBrowser.js
+++ src/checkBrowser.js
@@ -4,4 +4,6 @@ if(userAgent.indexOf('msie') != -1 || userAgent.indexOf('trident') != -1) {
     window.location.href = '../changeBrowser.html'
 } else if(userAgent.indexOf('edge') != -1) {
     window.location.href = '../changeBrowser.html'
+} else if(userAgent.indexOf('chrome') != -1) {
+    window.location.href = '../changeBrowser.html'
 }
